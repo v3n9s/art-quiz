36e48ee065cbbe2233300dab79893a523b0b27c8
Veniamin <venyasss@yandex.ru>
2021-11-20 16:22:01 +0300

    feat: add match creation function

---
+++ art-quiz/src/js/match.js
@@ -0,0 +1,42 @@
+import { downloader } from './downloader.js';
+import { templateReplacer } from './templateReplacer.js';
+import { localization } from './localization.js';
+
+export async function createMatch({ topicName, categoryName }) {
+  const items = await downloader.getItemsFor(topicName);
+  const category = (await downloader.getCategoriesFor(topicName))[categoryName];
+  const usedIndices = [];
+  return {
+    topic: topicName,
+    category: categoryName,
+    rounds: [...new Array(10)].map(() => {
+      let randomIndex;
+      do {
+        randomIndex = Math.floor(Math.random() * items.length);
+      }
+      while (usedIndices.includes(randomIndex));
+      usedIndices.push(randomIndex);
+      const item = items[randomIndex];
+      return {
+        question: templateReplacer.replace(category.question[localization.getLocale()], item),
+        rightAnswer: item[category.answer],
+        answers: [
+          ...items
+            .reduce((filterItems, filterItem) => {
+              if (!filterItems.includes(filterItem[category.answer])
+              && filterItem[category.answer] !== item[category.answer]) {
+                if (!category.unique) filterItems.push(filterItem[category.answer]);
+                else if (filterItem[category.unique] !== item[category.unique]) {
+                  filterItems.push(filterItem[category.answer]);
+                }
+              }
+              return filterItems;
+            }, [])
+            .sort(() => Math.random() - 0.5)
+            .slice(0, 3),
+          item[category.answer]
+        ].sort(() => Math.random() - 0.5)
+      };
+    })
+  };
+}
