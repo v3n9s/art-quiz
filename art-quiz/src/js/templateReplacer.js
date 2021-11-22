b74bb580169b882d9f05a133def929c5639b5ef8
Veniamin <venyasss@yandex.ru>
2021-11-23 01:02:21 +0300

    fix: add handler for empty template

--- art-quiz/src/js/templateReplacer.js
+++ art-quiz/src/js/templateReplacer.js
@@ -1,5 +1,6 @@
 export const templateReplacer = {
   replace(template, context) {
+    if (!template) return '';
     return Object.values(template.match(/(?<={{).*?(?=}})/g))
       .reduce((str, key) => {
         return str.replace(`{{${key}}}`, context[key]);
