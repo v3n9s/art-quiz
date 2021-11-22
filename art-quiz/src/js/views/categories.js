363678238b753d7fe6a29c8b2b91f37a2bad2b8c
Veniamin <venyasss@yandex.ru>
2021-11-23 00:20:46 +0300

    feat: add separate image for each category

--- art-quiz/src/js/views/categories.js
+++ art-quiz/src/js/views/categories.js
@@ -1,20 +1,21 @@
 import { View } from '../view.js';
 import { downloader } from '../downloader.js';
 import { localization } from '../localization.js';
+import { templateReplacer } from '../templateReplacer.js';
 
 export const view = new View({
   viewName: 'categories',
   funcs: {
     async getContext({ parameters }) {
-      const topics = await downloader.getTopics();
       const categories = await downloader.getCategoriesFor(parameters.topic);
+      const items = await downloader.getItemsFor(parameters.topic);
       return {
         categories: Object.entries(categories)
           .map(([key, info]) => {
             return `
             <li class="cards__item">
               <a class="card__link" href="#topics/${parameters.topic}/categories/${key}/rounds/0">
-                <img class="card__image" src="${topics[parameters.topic].imageUrl}">
+                <img class="card__image" src="${templateReplacer.replace(categories[key].baseUrl, { itemInd: Math.floor(Math.random() * items.length) })}">
                 ${info.name[localization.getLocale()]}
               </a>
             </li>`;
